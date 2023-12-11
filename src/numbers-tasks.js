2e946bdbd20504a1627f8d77254d392a58f400f1
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2023-12-11 23:59:50 +0400

    add task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -475,8 +475,8 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
@@ -490,8 +490,11 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  if (Number.isSafeInteger(number)) {
+    return true;
+  }
+  return false;
 }
 
 /**
